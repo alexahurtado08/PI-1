98d5a7ca4d00450fbf11dbe2f49fc1eb7101f2e6
= <=>
2025-02-17 22:44:30 -0500

    creacion modelo Alert

--- security/models.py
+++ security/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Alert(models.Model):
+    STATUS_CHOICES = [
+        ('ACTIVE', 'Activa'),
+        ('RESOLVED', 'Resuelta'),
+    ]
+    sensor_id = models.CharField(max_length=100, help_text="ID del sensor que activó la alarma")
+    triggered_at = models.DateTimeField(auto_now_add=True, help_text="Fecha y hora en que se activó la alarma")
+    location = models.CharField(max_length=255, help_text="Ubicación del sensor")
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='ACTIVE', help_text="Estado de la alarma")
+    additional_info = models.TextField(blank=True, null=True, help_text="Información adicional del evento")
