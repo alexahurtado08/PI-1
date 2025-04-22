f1131b298ffc69679dfa7629f89ac219326cfc8e
Mariana Valderrama Castañeda <mvaderrc1@eafit.edu.co>
2025-04-21 19:32:45 -0500

    bd computadores

--- security/views.py
+++ security/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from user.ComputerForm import ComputerForm 
 
 
-# Create your views here.
+def registrar_computadora(request):
+    if request.method == 'POST':
+        form = ComputerForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_computadoras')  # Reemplaza con tu URL correcta
+    else:
+        form = ComputerForm()
+    return render(request, 'security/registrar_computadora.html', {'form': form})
