467e9c19c4f3e3385079e30b24334efa0b9652f2
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-04-22 19:21:55 -0500

    Correción admin.py

--- administration/admin.py
+++ administration/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from administration.models import Alert
-from administration.models import Fotografia
-from administration.models import AuthorizedPersonnel
+from .models import Alert
+from .models import Fotografia
+from user.models import AuthorizedPersonnel
 from administration.models import Computer
 
 # Register your models here.
