b0cc968dea07264b138409bd3e4a20473e73c15a
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-04-22 19:27:08 -0500

    Correción admin.py(Security)

--- administration/admin.py
+++ administration/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from .models import Alert
-from security.models import Fotografia, Computer
-from user.models import AuthorizedPersonnel
+
 
 
 # Register your models here.
 admin.site.register(Alert)
-admin.site.register(Fotografia)
-admin.site.register(AuthorizedPersonnel)
-admin.site.register(Computer)
+
--- security/admin.py
+++ security/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Fotografia 
+from .models import Fotografia, Computer
 from user.models import AuthorizedPersonnel
 
 # Register your models here.
 admin.site.register(Fotografia)
 admin.site.register(AuthorizedPersonnel)
+admin.site.register(Computer)
 
