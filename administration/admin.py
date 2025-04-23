d613d5c0dfe215aae6f57f45f1f4eb111eae141f
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-04-22 19:19:56 -0500

    Cambios admin.py

--- administration/admin.py
+++ administration/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from administration.models import Alert
+from administration.models import Fotografia
+from administration.models import AuthorizedPersonnel
+from administration.models import Computer
 
 # Register your models here.
 admin.site.register(Alert)
+admin.site.register(Fotografia)
+admin.site.register(AuthorizedPersonnel)
+admin.site.register(Computer)
