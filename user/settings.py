215a56f75a6f8044e4789772fa2d269be573cf69
alexahurtado08 <ahurtadod@eafit.edu.co>
2025-05-28 14:24:22 -0500

    Actualización Ip

--- user/settings.py
+++ user/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-j)ppf!aq6lv22vve!!ai^*o_%yj+$lp!oi(2(8hl=ehk$f@!lv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.91.212.193']
+ALLOWED_HOSTS = ['3.84.92.230']
 
 
 # Application definition
